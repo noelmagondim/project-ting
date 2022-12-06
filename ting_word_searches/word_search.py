6823f52f5f0810496145cf094d38e3f27f2b4091
noelmagondim <noelmagondim@gmail.com>
2022-12-06 18:32:19 -0300

    Req 06: concluded

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrencies = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+
+        data = [
+            {'linha': (index + 1)}
+            for index, line in enumerate(file['linhas_do_arquivo'])
+            if word.lower() in line.lower()
+        ]
+
+        new_occurrencies = {
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': data,
+        }
+
+        if len(new_occurrencies['ocorrencias']) <= 0:
+            return []
+        else:
+            occurrencies.append(new_occurrencies)
+        return occurrencies
 
 
 def search_by_word(word, instance):
