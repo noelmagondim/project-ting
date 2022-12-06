952e90fae0a8f25303840c7351317eec2a71a69c
noelmagondim <noelmagondim@gmail.com>
2022-12-06 17:49:54 -0300

    Req 05: concluded

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -28,4 +28,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        info = instance.search(position)
+        sys.stdout.write(str(info))
+    except IndexError:
+        sys.stderr.write('Posição inválida\n')
