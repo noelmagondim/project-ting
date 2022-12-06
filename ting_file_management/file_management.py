095afbbe1f5304b608775efa8a7993891b01178b
noelmagondim <noelmagondim@gmail.com>
2022-12-06 10:54:05 -0300

    Req 02: concluded

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        try:
+            with open(path_file) as file:
+                return file.read().splitlines()
+        except IOError:
+            sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        return sys.stderr.write('Formato inválido\n')
