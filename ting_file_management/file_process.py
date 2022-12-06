9f3c17eb9784fe43226802a19eb85910e32d1bda
noelmagondim <noelmagondim@gmail.com>
2022-12-06 11:20:40 -0300

    Req 03: concluded

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+    instance.enqueue(new_dict)
+    print(new_dict)
 
 
 def remove(instance):
