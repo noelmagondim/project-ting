d3cde8160b933aa7b257ab826928f12621871e1e
noelmagondim <noelmagondim@gmail.com>
2022-12-06 11:29:11 -0300

    Req 04: concluded

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,9 +6,9 @@ from ting_file_management.file_management import txt_importer
 def process(path_file, instance):
     data = txt_importer(path_file)
     new_dict = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(data),
-        "linhas_do_arquivo": data,
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data,
     }
     for index in range(len(instance)):
         if instance.search(index)['nome_do_arquivo'] == path_file:
@@ -18,7 +18,13 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        return sys.stdout.write('Não há elementos\n')
+    else:
+        del_queue = instance.dequeue()
+        sys.stdout.write(
+            f'Arquivo {del_queue["nome_do_arquivo"]} removido com sucesso\n'
+        )
 
 
 def file_metadata(instance, position):
